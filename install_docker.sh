db5289c9d7ade8a596908fe72db97ed24a91d224
Thuong Dinh <thuong.dinhhoang@asnet.com.vn>
2015-09-29 15:32:29 +0700

    Update install docker

--- install_docker.sh
+++ install_docker.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-curl -s https://get.docker.io/ubuntu/ | sudo sh
+apt-get update
+apt-get -y install docker.io
+ln -sf /usr/bin/docker.io /usr/local/bin/docker
+sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
